99bbd5b54c64ca53f21be5073368fe5a294a5dd1
Almasbek010 <almasbekzhastar@gmail.com>
2026-01-27 17:25:13 +0500

    W3Schools tasks

---
+++ W3Schools/strings.py
@@ -0,0 +1,26 @@
+a = "Hello, World!"
+print(a.strip()) # returns "Hello, World!"
+
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
